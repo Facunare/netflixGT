047f9765faaa7782cb552c85538f7a3b7a43f6c8
facunare <arechaga.facundoet36@gmail.com>
2023-09-29 14:50:00 -0300

    preparando deploy

--- proyectardo/src/nodeApp/mongo.js
+++ proyectardo/src/nodeApp/mongo.js
@@ -5,8 +5,9 @@ import cookieParser from "cookie-parser";
 import cors from 'cors'
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://netflix-gt.vercel.app',
     credentials: true,
+    methods: ['POST', 'GET']
 }))
 app.use(morgan('dev'))
 app.use(express.json())
