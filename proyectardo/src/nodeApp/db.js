c7e33c6c4768c0c5afd0d22862c98e57b13e105b
facunare <arechaga.facundoet36@gmail.com>
2023-09-29 16:00:06 -0300

    preparando deploy

--- proyectardo/src/nodeApp/db.js
+++ proyectardo/src/nodeApp/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
     
   try {
-    await mongoose.connect('mongodb+srv://arechagafacundoet36:<Tolchaga500>@cluster0.qmgg2lh.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://arechagafacundoet36:<S9qpkv2hVkyFdE8j>@cluster0.qmgg2lh.mongodb.net/?retryWrites=true&w=majority');
     console.log('Connected to MongoDB');
   } catch (e) {
     console.error('Error connecting to MongoDB:', e);
